576f2125542771c6895d384bc4ce61236312294b
Patrick Harrison <jahman@bellatlantic.net>
2019-08-26 01:49:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,8 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 
- def initialize
+ def initialize (name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, 
+wrist_size:, glove_size:, pant_length:, pant_width:)
    
    
    
